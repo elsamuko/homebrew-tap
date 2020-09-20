bd2b255a5d5d5418406c59be929eb5b2cb3cdcaf
Samuel Albrecht <elsamuko@gmail.com>
2020-09-20 12:01:41 +0200

    fix: shasum for cppsh.rb download

--- cppsh.rb
+++ cppsh.rb
@@ -2,7 +2,7 @@ class Cppsh < Formula
   desc "C++ Scripting Interpreter"
   homepage "https://github.com/elsamuko/cppsh"
   url "https://github.com/elsamuko/cppsh/archive/v1.1.zip"
-  sha256 "0d5d6c01503443e9d1eab15b6a6211b4d457b3b3ddd905c69d48d4c8919d1964"
+  sha256 "0179b231c37ac08d19934df47c0a34ffdc066744d04355b13e03a3b0e72a8e99"
   license "GPL-3.0-or-later"
   head "https://github.com/elsamuko/cppsh.git"
 
