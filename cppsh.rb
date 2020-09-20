39355acf52a3b90046ace15c6bf9152e69fd1973
Samuel Albrecht <elsamuko@gmail.com>
2020-09-20 11:16:49 +0200

    chg: updated cppsh to v1.1

--- cppsh.rb
+++ cppsh.rb
@@ -1,8 +1,8 @@
 class Cppsh < Formula
   desc "C++ Scripting Interpreter"
   homepage "https://github.com/elsamuko/cppsh"
-  url "https://github.com/elsamuko/cppsh/archive/v1.0.zip"
-  sha256 "988a6f820bc40e74631e5309f9414fdcab15d68241de66143f1c7d627e5cf3e6"
+  url "https://github.com/elsamuko/cppsh/archive/v1.1.zip"
+  sha256 "0d5d6c01503443e9d1eab15b6a6211b4d457b3b3ddd905c69d48d4c8919d1964"
   license "GPL-3.0-or-later"
   head "https://github.com/elsamuko/cppsh.git"
 
