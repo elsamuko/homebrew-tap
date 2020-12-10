02d1e705da0da917c474ae90cdac738026c85585
Samuel Albrecht <elsamuko@gmail.com>
2020-12-10 22:39:23 +0100

    chg: fsrc 0.22

--- fsrc.rb
+++ fsrc.rb
@@ -1,8 +1,8 @@
 class Fsrc < Formula
   desc "Fast code search"
   homepage "https://github.com/elsamuko/fsrc"
-  url "https://github.com/elsamuko/fsrc/releases/download/v0.21/fsrc-mac-v0.21.zip"
-  sha256 "a51775baea2fb6afc092a9f24d372a5729fea096a910db0addc9c67e74817348"
+  url "https://github.com/elsamuko/fsrc/releases/download/v0.22/fsrc-mac-v0.22.zip"
+  sha256 "400082b78b458b62915046946c9323208988a8c2887d244a49752d4ac2fce913"
   license "GPL-3.0-or-later"
   head "https://github.com/elsamuko/fsrc.git"
 
